77e8fbe6f84b3877d41410e51260ef6a6a8169ce
jhrebenach <jhrebena@u.rochester.edu>
2016-04-09 20:00:33 -0400

    made master file
    not sure if it’s right though

---
+++ db-create-10.sql
@@ -0,0 +1,7 @@
+-- Master Document for Team 10
+-- Created on 4/9/2016
+
+\include /home/aincerto/db-create-10-aincerto.sql
+\include /home/jhrebena/db-create-10-jhrebena.sql
+\include /home/lpien/db-create-10-lpien.sql
+\include /home/mfloresk/db-create-10-mfloresk.sql
